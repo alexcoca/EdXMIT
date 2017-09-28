6dfd27515823a09bb12ac035c870378cc1ef9b8d
Alexandru Coca <alexcoca23@yahoo.co.uk>
2017-09-28 17:39:31 +0100

    Finalised code for Problem 2, Week 2

--- minPaymentWeek2.py
+++ minPaymentWeek2.py
@@ -5,15 +5,38 @@ Created on Thu Sep 28 15:25:14 2017
 @author: alexc
 """
 
-def minPayment():
-    pass
+import pdb
 
-month=1
-def balanceCalc(balance,annualInterestRate,monthlyPaymentRate,month):
+
+month = 1
+def balanceCalc(balance,annualInterestRate,monthlyPayment,month):
     if month == 12:
-        return round(balance - monthlyPaymentRate*balance + (annualInterestRate/12)*(balance-monthlyPaymentRate*balance),2)
+        return round(balance - monthlyPayment + (annualInterestRate/12)*(balance-monthlyPayment),2)
     else:
         month +=1
-        return balanceCalc(round(balance-monthlyPaymentRate*balance+(annualInterestRate/12)*(balance-monthlyPaymentRate*balance),2),annualInterestRate,monthlyPaymentRate,month)
+        return balanceCalc(round(balance-monthlyPayment+(annualInterestRate/12)*(balance-monthlyPayment),2),annualInterestRate,monthlyPayment,month)
 
-print("Remaining balance:"+" "+str(balanceCalc(balance,annualInterestRate,monthlyPaymentRate,month)))
+
+guess = balance/2
+upper_bound = balance
+lower_bound = 0
+last_guess = guess
+tol = 1e-7
+
+while (abs(balanceCalc(balance,annualInterestRate,guess,month))>tol):
+    month = 1;
+    if balanceCalc(balance,annualInterestRate,guess,month) > 0:
+        lower_bound = last_guess
+        guess = lower_bound + (upper_bound - lower_bound)/2
+        last_guess = guess
+    else:
+        upper_bound = last_guess
+        guess =upper_bound - (upper_bound - lower_bound)/2
+        last_guess = guess
+        
+guess = int(round(guess,0))
+
+while (guess%10 != 0):
+    guess += 1
+
+print("Lowest Payment:"+" "+str(guess))
