9b11798b268b0f983c118f74e7df6510f788f3f7
Alexandru Coca <alexcoca23@yahoo.co.uk>
2017-09-28 16:06:30 +0100

    Starting to develop the code for this on a separate branch

--- minPaymentWeek2.py
+++ minPaymentWeek2.py
@@ -7,3 +7,13 @@ Created on Thu Sep 28 15:25:14 2017
 
 def minPayment():
     pass
+
+month=1
+def balanceCalc(balance,annualInterestRate,monthlyPaymentRate,month):
+    if month == 12:
+        return round(balance - monthlyPaymentRate*balance + (annualInterestRate/12)*(balance-monthlyPaymentRate*balance),2)
+    else:
+        month +=1
+        return balanceCalc(round(balance-monthlyPaymentRate*balance+(annualInterestRate/12)*(balance-monthlyPaymentRate*balance),2),annualInterestRate,monthlyPaymentRate,month)
+
+print("Remaining balance:"+" "+str(balanceCalc(balance,annualInterestRate,monthlyPaymentRate,month)))
